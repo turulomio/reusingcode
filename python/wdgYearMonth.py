1abd899c46aa7682f2ba2345eda6e81275e0e608
turulomio <turulomio@yahoo.es>
2019-09-15 10:20:54 +0200

    Now emits changed better

--- python/wdgYearMonth.py
+++ python/wdgYearMonth.py
@@ -17,6 +17,7 @@ class wdgYearMonth(QWidget, Ui_wdgYearMonth):
         
     def initiate(self, firstyear,  lastyear, currentyear, currentmonth):
         """Debe ser la primera función después del constructor"""
+        self.blockSignals(True)
         if firstyear==None:
             self.setEnabled(False)
             print (function_name(self), "Firstyear is None")
@@ -28,13 +29,17 @@ class wdgYearMonth(QWidget, Ui_wdgYearMonth):
         self.month=currentmonth
         for year in range(firstyear, lastyear+1):
             self.cmbYear.addItem(str(year), year)
+        self.blockSignals(False)
         self.set(currentyear, currentmonth)
         
     def set(self,  year , month):
+        self.blockSignals(True)
         self.year=year
         self.month=month
         self.cmbYear.setCurrentIndex(self.year-self.firstyear)
         self.cmbMonth.setCurrentIndex(self.month-1)
+        self.blockSignals(False)
+        self.changed.emit()
 
     @pyqtSlot(str)      
     def on_cmbYear_currentIndexChanged(self, text):
