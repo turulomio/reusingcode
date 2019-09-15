cfd996d213c62849a71535a72a8e6674f36d55e2
turulomio <turulomio@yahoo.es>
2019-09-15 20:30:55 +0200

    Solving dependency

--- python/decorators.py
+++ python/decorators.py
@@ -1,6 +1,8 @@
 import time
 import warnings
 import functools
+import socket
+from logging import error, debug
 
 ## allows you to measure the execution time of the method/function by just adding the @timeit decorator on the method.
 ## @timeit
@@ -19,6 +21,19 @@ def timeit(method):
 
     return timed
 
+## Checks if there is internet
+def is_there_internet(method):
+    def internet(*args, **kw):
+        try:
+            # connect to the host -- tells us if the host is actually
+            # reachable
+            socket.create_connection(("www.google.com", 80))
+            debug("There is Internet connection")
+        except OSError:
+            error("There is not Internet connection")
+        return method(*args, **kw)
+    return internet
+
 def deprecated(func):
      """This is a decorator which can be used to mark functions
      as deprecated. It will result in a warning being emitted
@@ -32,3 +47,13 @@ def deprecated(func):
          warnings.simplefilter('default', DeprecationWarning)  # reset filter
          return func(*args, **kwargs)
      return new_func
+
+@is_there_internet
+@deprecated
+@timeit
+def testing_decorators():
+    for i in range(10):
+        print(i)
+        
+if __name__ == "__main__":
+    testing_decorators()
