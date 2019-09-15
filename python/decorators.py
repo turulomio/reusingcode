f6a23b7c378669a8e38703956e53152a8eb2af55
turulomio <turulomio@yahoo.es>
2019-09-15 20:41:01 +0200

    Improved decorators. Added internet

--- python/decorators.py
+++ python/decorators.py
@@ -1,16 +1,14 @@
-import time
-import warnings
-import functools
-import socket
-from logging import error, debug
+from time import time
+from socket import create_connection
+from logging import error, debug, warning
 
 ## allows you to measure the execution time of the method/function by just adding the @timeit decorator on the method.
 ## @timeit
 def timeit(method):
     def timed(*args, **kw):
-        ts = time.time()
+        ts = time()
         result = method(*args, **kw)
-        te = time.time()
+        te = time()
 
         if 'log_time' in kw:
             name = kw.get('log_name', method.__name__.upper())
@@ -27,33 +25,34 @@ def is_there_internet(method):
         try:
             # connect to the host -- tells us if the host is actually
             # reachable
-            socket.create_connection(("www.google.com", 80))
+            create_connection(("www.google.com", 80))
             debug("There is Internet connection")
         except OSError:
             error("There is not Internet connection")
         return method(*args, **kw)
     return internet
 
-def deprecated(func):
-     """This is a decorator which can be used to mark functions
-     as deprecated. It will result in a warning being emitted
-     when the function is used."""
-     @functools.wraps(func)
-     def new_func(*args, **kwargs):
-         warnings.simplefilter('always', DeprecationWarning)  # turn off filter
-         warnings.warn("Call to deprecated function {}.".format(func.__name__),
-                       category=DeprecationWarning,
-                       stacklevel=2)
-         warnings.simplefilter('default', DeprecationWarning)  # reset filter
-         return func(*args, **kwargs)
-     return new_func
+## This is a decorator which can be used to mark functions
+## as deprecated. It will result in a warning being emitted
+## when the function is used
+def deprecated(method):
+    def new_func(*args, **kwargs):
+        warning("Call to deprecated function {}.".format(method.__name__))
+        return method(*args, **kwargs)
+    return new_func
+
 
 @is_there_internet
 @deprecated
+def testing_decorators1():
+    for i in range(10):
+        print(i)
+
 @timeit
-def testing_decorators():
+def testing_decorators2():
     for i in range(10):
         print(i)
         
 if __name__ == "__main__":
-    testing_decorators()
+    testing_decorators1()
+    testing_decorators2()
