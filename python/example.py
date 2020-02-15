a2a3e5e9d9745f61402dee0922f0c4ad1bd418f4
turulomio <turulomio@yahoo.es>
2020-02-15 14:45:51 +0100

    Added frmAccess to example. frmAccess saves dialog size. Fixes #19

--- python/example.py
+++ python/example.py
@@ -6,6 +6,27 @@ def command(s):
     print (s)
     system(s)
 
+def frmAccess():
+    dir="/tmp/reusingcode_frmaccess"
+    rmtree(dir, ignore_errors=True)
+    makedirs(dir, exist_ok=True)
+
+    command("cp ui/{0}.py {1}/{0}.py".format(args.example,dir))
+    command("cp connection_pg.py {0}".format(dir))
+    command("cp connection_pg_qt.py {0}".format(dir))
+    command("cp translationlanguages.py {0}".format(dir))
+    command("cp package_resources.py {0}".format(dir))
+    command("cp libmanagers.py {0}".format(dir))
+    command("pyuic5 ui/{0}.ui -o {1}/Ui_{0}.py".format(args.example, dir))
+    command("sed -i -e 's/\.Ui/Ui/' {1}/{0}.py".format(args.example, dir))
+    command("sed -i -e 's/ \.\. / /' {1}/{0}.py".format(args.example, dir))
+    command("sed -i -e 's/ \.connection/ connection/' {}/connection_pg_qt.py".format(dir))
+    command("sed -i -e 's/ \.libmanagers/ libmanagers/' {}/translationlanguages.py".format(dir))
+    command("sed -i -e 's/ \.package/ package/' {}/translationlanguages.py".format(dir))
+    command("cp datetime_functions.py {0}/datetime_functions.py".format(dir))
+    command("sed -i -e 's/\.datetime_functions/datetime_functions/' {}/libmanagers.py".format(dir))
+    command("python {1}/{0}.py".format(args.example, dir))
+
 def wdgDatetime():
     dir="/tmp/reusingcode_wdgdatetime"
     rmtree(dir, ignore_errors=True)
@@ -66,11 +87,13 @@ def myqcharts():
 
 
 parser=ArgumentParser(description='Program to allow see reusingcode modules as standalone scripts', formatter_class=RawTextHelpFormatter)
-parser.add_argument('--example', action='store', choices=['wdgDatetime', 'frmSelector', 'myQTableWidget' , 'myqcharts'], required=True)
+parser.add_argument('--example', action='store', choices=['wdgDatetime', 'frmSelector', 'myQTableWidget' , 'myqcharts', 'frmAccess'], required=True)
 args=parser.parse_args()
 
 if args.example=="wdgDatetime":
     wdgDatetime()
+elif args.example=="frmAccess":
+    frmAccess()
 elif args.example=="frmSelector":
     frmSelector()
 elif args.example=="myQTableWidget":
